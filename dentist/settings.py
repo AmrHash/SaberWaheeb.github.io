dac5f4818789d01896c1db207afb7f321632347e
Amr Hashem <47293199+AmrHash@users.noreply.github.com>
2020-12-21 20:35:42 +0200

    Update settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'dentist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'personal_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Shithappens4',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        
     }
 }
 
